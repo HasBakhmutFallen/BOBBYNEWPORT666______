f2bb8f30647f0f70720e48b5310a47264156204c
bobbynewport666 <61870158+bobbynewport666@users.noreply.github.com>
2023-03-19 14:48:35 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-  const noLink = document.querySelector("#no-link");
-  noLink.addEventListener("click", function(event) {
-    event.preventDefault();
-    alert("No, Bakhmut has not fallen.");
-  });
-};
